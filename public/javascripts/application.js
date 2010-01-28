a5abe4ec1e46a4d1e9fdd4016d65153329134e1f
seb <seb@tao.panter.local>
2010-01-28 15:43:13 +0100

    added new views and workload calculation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -34,6 +34,7 @@ function generate_css() {
 function filter_view() {
   filter.set(this.name, this.checked);
   scheduler.update_view();
+  recalculate_stats();
 }
 
 function generate_filter() {
@@ -41,28 +42,106 @@ function generate_filter() {
     var label = new Element('label', { class: person});
     var check = new Element('input', {type:"checkbox", checked:"true", name:person});
     check.observe("change", filter_view);
-    label.update(check);
-    label.insert(person);
+    label.update(person+': ');
+    label.insert(check);
     $('filter').insert(label);
   });
 }
 
+function update_event_text(event_id, event) {
+  event.text = event.person + "\n" + event.task;
+}
+
+function recalculate_stats() {
+  var workload = new Hash();
+  var projects = new Hash();
+  var total = 0;
+  scheduler.get_visible_events().each(function(e) {
+    var hours = (e.end_date.getTime() - e.start_date.getTime()) / 3600000.0
+    workload.set(e.person, hours + (workload.get(e.person) || 0));
+    projects.set(e.task, hours + (projects.get(e.task) || 0));
+    total = total + hours;
+  });
+
+  var dl = new Element('dl');
+  people.each(function(p) {
+    dl.insert(new Element('dt', { class: p }).update(p+':'));
+    dl.insert(new Element('dd', { class: p }).update((workload.get(p) || 0)+'h'));
+    });
+  dl.insert(new Element('dt').update('Total: '));
+  dl.insert(new Element('dd').update(total+'h'));
+  $('workload').update(dl);
+
+  var dl = new Element('dl');
+  projects.each(function(pair) {
+    dl.insert(new Element('dt').update(pair.key+':'));
+    dl.insert(new Element('dd').update(pair.value+'h '));
+    });
+  $('projects').update(dl);
+
+}
+
 function init_scheduler() {
-  scheduler.init('scheduler_here',new Date(2010,0,23),"week");
   scheduler.templates.event_class=function(start,end,event){
     return event.person;
   }
+  scheduler.config.lightbox.sections = [
+  			{name:"person", height:23, map_to:"person",  type:"textarea", focus: true },
+  			{name:"task", height:23, map_to:"task", type:"textarea" },
+				];
+  scheduler.config.details_on_create=true;
+  scheduler.config.details_on_dblclick=true;
+  scheduler.locale.labels.section_person="Who?"
+  scheduler.locale.labels.section_task="What?"
+
+
+  scheduler.attachEvent("onEventAdded", update_event_text);
+  scheduler.attachEvent("onEventChanged", update_event_text);
+
+  scheduler.config.first_hour =  8;
+  scheduler.config.last_hour  = 22;
+  scheduler.config.time_step  = 30;
+  scheduler.config.start_on_monday = true;
+
+  scheduler.config.icons_select=["icon_details","icon_delete"]
+
   scheduler.filter_week=function(id, event) {
     return filter.get(event.person);
   }
+  scheduler.filter_day=scheduler.filter_week;
+
+  scheduler.locale.labels.workweek_tab = "Work";
+
+  scheduler.attachEvent("onTemplatesReady",function(){
+    //work week
+    scheduler.date.workweek_start = scheduler.date.week_start;
+    scheduler.templates.workweek_date = scheduler.templates.week_date;
+    scheduler.templates.workweek_scale_date = scheduler.templates.week_scale_date;
+    scheduler.date.add_workweek=function(date,inc){ return scheduler.date.add(date,inc*7,"day"); }
+    scheduler.date.get_workweek_end=function(date){ return scheduler.date.add(date,5,"day"); }
+    scheduler.filter_workweek=scheduler.filter_week;
+  });
+
+  scheduler.init('scheduler_here', null, "workweek");
 }
 
 function load_data() {
   // demo data
-  people.each(function(person) {
-    scheduler.addEvent({ start_date:new Date(2010,0,23,2,0), end_date:new Date(2010,0,23,3,0), text:person, person:person });
-    scheduler.addEvent({ start_date:new Date(2010,0,23,4,0), end_date:new Date(2010,0,23,5,0), text:person, person:person });
-  });
+  // people.each(function(person) {
+  //   scheduler.addEvent({ start_date:new Date(2010,0,21,9,0), end_date:new Date(2010,0,21,11,0), person:person, task:'' });
+  //   scheduler.addEvent({ start_date:new Date(2010,0,21,12,0), end_date:new Date(2010,0,21,17,0), person:person, task:'' });
+  // });
+}
+
+function init_stats() {
+  scheduler.attachEvent("onBeforeEventDelete", function() {
+      setTimeout('recalculate_stats()', 1000);
+      return true;
+      });
+  scheduler.attachEvent("onEventAdded", recalculate_stats);
+  scheduler.attachEvent("onEventChanged", recalculate_stats);
+  scheduler.attachEvent("onViewChange", recalculate_stats);
+  recalculate_stats();
 }
 
 document.observe("dom:loaded", function() {
@@ -70,4 +149,5 @@ document.observe("dom:loaded", function() {
   generate_filter();
   init_scheduler();
   load_data();
+  init_stats();
 });
